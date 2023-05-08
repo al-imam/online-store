4a6650c02a464ab22dff3f794a2f26c0299eb138
al-imam <alimam01828@gmail.com>
2023-05-08 23:20:14 +0600

    convert server response to ts type

--- components/products/ListProduct.tsx
+++ components/products/ListProduct.tsx
@@ -1,8 +1,35 @@
 import type { FunctionComponent } from "react";
 import React from "react";
 
+interface Image {
+  product_id: string;
+  url: string;
+}
+
+interface Review {
+  rating: string;
+  comment: string;
+  created: Date;
+}
+
+interface ServerResponse {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  seller: string;
+  stock: number;
+  rating: number;
+  category: string;
+  images: Image[];
+  reviews: Review[];
+  created: string;
+  updated: string;
+  __v: number;
+}
+
 interface ListProductProps {
-  data: any;
+  data: ServerResponse[];
 }
 
 const ListProduct: FunctionComponent<ListProductProps> = ({ data }) => {
