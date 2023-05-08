aa753f5c290fc1f0514356ba3eb7960fe13d3dce
al-imam <alimam01828@gmail.com>
2023-05-08 17:39:55 +0600

    create createProduct controller

---
+++ backend/controllers/productController.ts
@@ -0,0 +1,7 @@
+import Product from "../models/product";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export async function create(req: NextApiRequest, res: NextApiResponse) {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
+}
