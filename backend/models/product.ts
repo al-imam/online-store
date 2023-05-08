92b8d6cb4ee47529c68cd751be5d75998b6bb618
al-imam <alimam01828@gmail.com>
2023-05-08 16:40:03 +0600

    create product model add time info

--- backend/models/product.ts
+++ backend/models/product.ts
@@ -1,68 +1,74 @@
-import { Schema } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-const Product = new Schema({
-  name: {
-    type: String,
-    required: [true, "product required name"],
-  },
-
-  description: {
-    type: String,
-    required: [true, "product required description"],
-  },
-
-  price: {
-    type: Number,
-    required: [true, "product required price"],
-  },
-
-  images: [
-    {
-      product_id: String,
-      url: String,
+const Product = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "product required name"],
     },
-  ],
 
-  seller: {
-    type: String,
-    required: [true, "product required seller"],
-  },
+    description: {
+      type: String,
+      required: [true, "product required description"],
+    },
 
-  stock: {
-    type: Number,
-    required: [true, "product required stock"],
-  },
+    price: {
+      type: Number,
+      required: [true, "product required price"],
+    },
 
-  rating: {
-    type: Number,
-    default: 0,
-  },
-
-  reviews: [
-    {
-      rating: {
-        type: Number,
-        required: [true, "reviews required rating"],
+    images: [
+      {
+        product_id: String,
+        url: String,
       },
+    ],
 
-      comment: {
-        type: String,
-        required: [true, "reviews required comment"],
+    seller: {
+      type: String,
+      required: [true, "product required seller"],
+    },
+
+    stock: {
+      type: Number,
+      required: [true, "product required stock"],
+    },
+
+    rating: {
+      type: Number,
+      default: 0,
+    },
+
+    reviews: [
+      {
+        rating: {
+          type: Number,
+          required: [true, "reviews required rating"],
+        },
+
+        comment: {
+          type: String,
+          required: [true, "reviews required comment"],
+        },
+
+        created: {
+          type: Date,
+          default: Date.now,
+        },
       },
+    ],
 
-      created: {
-        type: Date,
-        default: Date.now,
+    category: {
+      type: String,
+      required: [true, "product required category"],
+      enum: {
+        values: ["electronics", "cameras", "laptops", "headphones", "sports"],
+        message: "select valid product category",
       },
     },
-  ],
-
-  category: {
-    type: String,
-    required: [true, "product required category"],
-    enum: {
-      values: ["electronics", "cameras", "laptops", "headphones", "sports"],
-      message: "select valid product category",
-    },
   },
-});
+
+  { timestamps: { createdAt: "created" } }
+);
+
+export default model("Product", Product);
