d1f189a6c8b5d17504ca63aad8a752569b1a1afa
al-imam <alimam01828@gmail.com>
2023-05-08 16:33:24 +0600

    create product schema

---
+++ backend/models/product.ts
@@ -0,0 +1,68 @@
+import { Schema } from "mongoose";
+
+const Product = new Schema({
+  name: {
+    type: String,
+    required: [true, "product required name"],
+  },
+
+  description: {
+    type: String,
+    required: [true, "product required description"],
+  },
+
+  price: {
+    type: Number,
+    required: [true, "product required price"],
+  },
+
+  images: [
+    {
+      product_id: String,
+      url: String,
+    },
+  ],
+
+  seller: {
+    type: String,
+    required: [true, "product required seller"],
+  },
+
+  stock: {
+    type: Number,
+    required: [true, "product required stock"],
+  },
+
+  rating: {
+    type: Number,
+    default: 0,
+  },
+
+  reviews: [
+    {
+      rating: {
+        type: Number,
+        required: [true, "reviews required rating"],
+      },
+
+      comment: {
+        type: String,
+        required: [true, "reviews required comment"],
+      },
+
+      created: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+
+  category: {
+    type: String,
+    required: [true, "product required category"],
+    enum: {
+      values: ["electronics", "cameras", "laptops", "headphones", "sports"],
+      message: "select valid product category",
+    },
+  },
+});
