a2ec246d95e364adec0f576b1de023da46e2b5ce
al-imam <alimam01828@gmail.com>
2023-05-19 00:58:03 +0600

    map over images

--- components/utility/ImageGallery.tsx
+++ components/utility/ImageGallery.tsx
@@ -1,7 +1,7 @@
-import { Image } from "@/types/productInterface";
+import { Image as ImageType } from "@/types/productInterface";
 
 interface ImageGalleryProps {
-  images: Image[];
+  images: ImageType[];
 }
 
 export default ({ images }: ImageGalleryProps) => {
@@ -17,15 +17,17 @@ export default ({ images }: ImageGalleryProps) => {
         />
       </div>
       <div className="space-x-2 overflow-auto text-center whitespace-nowrap">
-        <a className="inline-block border border-gray-200 p-1 rounded-md hover:border-blue-500 cursor-pointer">
-          <img
-            className="w-14 h-14"
-            src={"/logo192.png"}
-            alt="Product title"
-            width="500"
-            height="500"
-          />
-        </a>
+        {images.map((image) => (
+          <a className="inline-block border border-gray-200 p-1 rounded-md hover:border-blue-500 cursor-pointer">
+            <img
+              className="w-14 h-14"
+              src={image.url}
+              alt="Product title"
+              width="50"
+              height="50"
+            />
+          </a>
+        ))}
       </div>
     </aside>
   );
