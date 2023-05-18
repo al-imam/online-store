d9d7d2ec0876a8b50ef80e87221f992a48507226
al-imam <alimam01828@gmail.com>
2023-05-19 02:28:19 +0600

    map over list and show in ui

--- components/layout/BreadCrumbs.tsx
+++ components/layout/BreadCrumbs.tsx
@@ -11,12 +11,17 @@ const BreadCrumbs = ({ list }: BreadCrumbsProps) => (
   <section className="py-5 sm:py-7 bg-blue-100">
     <div className="container max-w-screen-xl mx-auto px-4">
       <ol className="inline-flex flex-wrap text-gray-600 space-x-1 md:space-x-3 items-center">
-        <li className="inline-flex items-center">
-          <Link href="/" className="text-gray-600 hover:text-blue-600">
-            Home
-          </Link>
-          <i className="ml-3 text-gray-400 fa fa-chevron-right"></i>
-        </li>
+        {list.map((object, id) => (
+          <li className="inline-flex items-center" key={id}>
+            <Link
+              href={object.url}
+              className="text-gray-600 hover:text-blue-600"
+            >
+              {object.name}
+            </Link>
+            <i className="ml-3 text-gray-400 fa fa-chevron-right"></i>
+          </li>
+        ))}
       </ol>
     </div>
   </section>
