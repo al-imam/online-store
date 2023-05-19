f44eb0a45ee4a8d69f4aa331615ec97e0c5c1a40
al-imam <alimam01828@gmail.com>
2023-05-20 00:41:49 +0600

    implement lazy load in backend

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -3,13 +3,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { isValidObjectId } from "mongoose";
 import filter from "../util/filter";
 
+function parseNumber(num: string, fallback: number) {
+  const n = parseInt(num as string);
+  if (!isNaN(n)) return n;
+  return fallback;
+}
+
 export async function createProduct(req: NextApiRequest, res: NextApiResponse) {
   const newProduct = await Product.create(req.body);
   res.status(201).json(newProduct);
 }
 
 export async function getAllProduct(req: NextApiRequest, res: NextApiResponse) {
-  const allProduct = await Product.find(filter(req.query));
+  const allProduct = await Product.find(filter(req.query), undefined, {
+    skip: parseNumber(req.query.skip as string, 0),
+    limit: 3,
+  });
   res.status(200).json(allProduct);
 }
 
