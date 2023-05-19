7ef4f9c3dddac6ca3cb2b1a4b849a7aae27e2345
al-imam <alimam01828@gmail.com>
2023-05-19 15:48:54 +0600

    implement filter search

--- backend/util/filter.ts
+++ backend/util/filter.ts
@@ -2,16 +2,25 @@ function isEmpty(o: object): boolean {
   return Object.keys(o).length === 0;
 }
 
-function filter(
-  query: Partial<{
-    search: string;
-    min: string;
-    max: string;
-    category: string;
-    rating: string;
-  }>
-) {
-  if (isEmpty(query)) return {};
+interface filterQuery {
+  search: string;
+  min: string;
+  max: string;
+  category: string;
+  rating: string;
+}
+
+function filter(query: Partial<filterQuery>) {
+  const q: any = {};
+  if (isEmpty(query)) return q;
+
+  if (query.search) {
+    q.name = { $regex: query.search, $options: "i" };
+  }
+
+  console.log(q);
+
+  return q;
 }
 
 export default filter;
