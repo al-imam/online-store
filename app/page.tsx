6c5d1fd1e87e8b5f731b9d1c9c5b4b574062e1b5
al-imam <alimam01828@gmail.com>
2023-05-20 00:12:49 +0600

    get filter data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { Get } from "@/utility/request";
 import ServerResponse from "@/types/productInterface";
 import ListProduct from "@/components/products/ListProduct";
+import QueryInterface from "@/types/queryInterface";
+import stringifyQuery from "@/utility/stringifyQuery";
 
-export default async function Home() {
-  const { data } = await Get<ServerResponse[]>("product");
+interface HomeProps {
+  searchParams: Partial<QueryInterface>;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const { data } = await Get<ServerResponse[]>(
+    `product/?${stringifyQuery(searchParams)}`
+  );
 
   return <ListProduct data={data} />;
 }
