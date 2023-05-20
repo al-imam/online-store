3216e70428505cceaa00b96ed8a9d4889f91542d
al-imam <alimam01828@gmail.com>
2023-05-20 19:24:33 +0600

    control min and max number input

--- components/layout/Filters.tsx
+++ components/layout/Filters.tsx
@@ -6,12 +6,18 @@ import useObjectStore from "use-object-store";
 import { useRouter } from "next/navigation";
 
 const categories = ["Electronics", "Laptops", "Toys", "Office", "Beauty"];
+const prices = [
+  { ui: "Min", store: "min" },
+  { ui: "Max", store: "max" },
+] as const;
 
 type SN = string | null;
 
 interface Store {
   category: SN;
   rating: SN;
+  min: number | undefined;
+  max: number | undefined;
 }
 
 type FixQueryProblem = { rating: SN } | { category: SN };
@@ -21,6 +27,8 @@ const Filters = () => {
   const [store, updateStore] = useObjectStore<Store>({
     category: null,
     rating: null,
+    max: undefined,
+    min: undefined,
   });
 
   function changeQuery(select: string, query: "category" | "rating") {
@@ -37,6 +45,18 @@ const Filters = () => {
     router.push(`/?${qp.toString()}`);
   }
 
+  const minmax = {
+    min(number: number) {
+      if (isNaN(number)) updateStore({ min: undefined });
+      updateStore({ min: number });
+    },
+
+    max(number: number) {
+      if (isNaN(number)) updateStore({ max: undefined });
+      updateStore({ max: number });
+    },
+  };
+
   return (
     <aside className="md:w-1/3 lg:w-1/4 px-4">
       <a
@@ -48,23 +68,18 @@ const Filters = () => {
       <div className="hidden md:block px-6 py-4 border border-gray-200 bg-white rounded shadow-sm">
         <h3 className="font-semibold mb-2">Price ($)</h3>
         <div className="grid md:grid-cols-3 gap-x-2">
-          <div className="mb-4">
-            <input
-              name="min"
-              className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
-              type="number"
-              placeholder="Min"
-            />
-          </div>
-
-          <div className="mb-4">
-            <input
-              name="max"
-              className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
-              type="number"
-              placeholder="Max"
-            />
-          </div>
+          {prices.map((v) => (
+            <div className="mb-4">
+              <input
+                name={v.store}
+                className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
+                type="number"
+                onChange={(e) => minmax[v.store](e.target.valueAsNumber)}
+                value={store[v.store]}
+                placeholder={v.ui}
+              />
+            </div>
+          ))}
 
           <div className="mb-4">
             <button className="px-1 py-2 text-center w-full inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700">
