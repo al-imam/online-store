cd435e2135a9d84f9a67e21a4c6382a8ccbdf4eb
al-imam <alimam01828@gmail.com>
2023-05-22 02:58:38 +0600

    boost performance convert forEach to for of
      - if any issue found go for next validation

--- backend/middleware/validateBody.ts
+++ backend/middleware/validateBody.ts
@@ -11,44 +11,49 @@ interface ObjectValidate {
 
 type ItemType = string | [string, ValidateFunction] | ObjectValidate;
 
-function validateBody(items: ItemType[], config = { strict: true }) {
+function validateBody(validators: ItemType[], config = { strict: true }) {
   return wrap(
     async (req: NextApiRequest, res: NextApiResponse, next: NextHandler) => {
-      const missingItems: any[] = [];
+      const missingItems: string[] = [];
 
-      items.forEach((item) => {
-        if (typeof item === "string") {
-          if (!req.body.hasOwnProperty(item)) {
-            missingItems.push(item);
+      for (const validator of validators) {
+        if (typeof validator === "string") {
+          if (!req.body.hasOwnProperty(validator)) {
+            missingItems.push(validator);
+            continue;
           }
         }
 
-        if (Array.isArray(item)) {
-          if (!req.body.hasOwnProperty(item[0]) && config.strict) {
-            missingItems.push(item[0]);
+        if (Array.isArray(validator)) {
+          if (!req.body.hasOwnProperty(validator[0]) && config.strict) {
+            missingItems.push(validator[0]);
+            continue;
           }
 
-          if (!item[1](req.body[item[0]])) {
-            missingItems.push(item[0]);
+          if (!validator[1](req.body[validator[0]])) {
+            missingItems.push(validator[0]);
+            continue;
           }
         }
 
-        if (item instanceof Object && !Array.isArray(item)) {
-          if (!req.body.hasOwnProperty(item.property) && config.strict) {
-            missingItems.push(item);
+        if (validator instanceof Object && !Array.isArray(validator)) {
+          if (!req.body.hasOwnProperty(validator.property) && config.strict) {
+            missingItems.push(validator.property);
+            continue;
           }
 
-          if (!item.validate(req.body[item.property])) {
-            missingItems.push(item);
+          if (!validator.validate(req.body[validator.property])) {
+            missingItems.push(validator.property);
+            continue;
           }
         }
-      });
+      }
 
       if (missingItems.length === 0) return next();
 
       return res.status(400).json({
         message: "some properties are missing or not valid in request body",
-        "missing-property": missingItems,
+        "missing-properties": missingItems,
       });
     },
     "validate-body"
