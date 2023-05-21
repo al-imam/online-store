7f4655d10ba56f491b7f47b762cbbfe561209728
al-imam <alimam01828@gmail.com>
2023-05-22 03:17:19 +0600

    send invalid items

--- backend/middleware/validateBody.ts
+++ backend/middleware/validateBody.ts
@@ -15,45 +15,41 @@ function validateBody(validators: ItemType[], config = { strict: true }) {
   return wrap(
     async (req: NextApiRequest, res: NextApiResponse, next: NextHandler) => {
       const missingItems: string[] = [];
+      const invalidItems: string[] = [];
 
       for (const validator of validators) {
         if (typeof validator === "string") {
-          if (!req.body.hasOwnProperty(validator)) {
-            missingItems.push(validator);
-            continue;
-          }
+          if (req.body.hasOwnProperty(validator)) continue;
+          missingItems.push(validator);
         }
 
         if (Array.isArray(validator)) {
           if (!req.body.hasOwnProperty(validator[0]) && config.strict) {
             missingItems.push(validator[0]);
-            continue;
           }
 
           if (!validator[1](req.body[validator[0]])) {
-            missingItems.push(validator[0]);
-            continue;
+            invalidItems.push(validator[0]);
           }
         }
 
         if (validator instanceof Object && !Array.isArray(validator)) {
           if (!req.body.hasOwnProperty(validator.property) && config.strict) {
             missingItems.push(validator.property);
-            continue;
           }
 
           if (!validator.validate(req.body[validator.property])) {
-            missingItems.push(validator.property);
-            continue;
+            invalidItems.push(validator.property);
           }
         }
       }
 
-      if (missingItems.length === 0) return next();
+      if (missingItems.length === 0 && invalidItems.length === 0) return next();
 
       return res.status(400).json({
-        message: "some properties are missing or not valid in request body",
+        message: "some properties are missing or invalid in request body",
         "missing-properties": missingItems,
+        "invalid-properties": invalidItems,
       });
     },
     "validate-body"
