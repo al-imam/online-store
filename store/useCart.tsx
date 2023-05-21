e6380bdd6f9ab4b96e83aaf510d8311fc32fa77f
al-imam <alimam01828@gmail.com>
2023-05-21 21:36:28 +0600

    if item is already exist then update the quantity

--- store/useCart.tsx
+++ store/useCart.tsx
@@ -4,12 +4,29 @@ import CartItemInterface from "@/types/cartItemInterface";
 interface StoreInterface {
   items: CartItemInterface[];
   addItem(item: CartItemInterface): void;
+  updateItem(id: string): void;
 }
 
-const useCart = create<StoreInterface>((set) => ({
+const useCart = create<StoreInterface>((set, get) => ({
   items: [],
+
   addItem(item: CartItemInterface) {
-    return set((store) => ({ items: [...store.items, item] }));
+    const { items, updateItem } = get();
+    const update = items.find((e) => e.id === item.id);
+    if (update) return updateItem(update.id);
+    set({ items: [...items, item] });
+  },
+
+  updateItem(id: string) {
+    set((store) => ({
+      items: store.items.map((item) => {
+        if (item.id === id) {
+          item.quantity++;
+          return item;
+        }
+        return item;
+      }),
+    }));
   },
 }));
 
