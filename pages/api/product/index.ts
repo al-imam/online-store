b5c44a36e1dccd194fb7a40b58073012c5ee0ba4
al-imam <alimam01828@gmail.com>
2023-05-22 02:00:25 +0600

    guard create product with validateBody HOC

--- pages/api/product/index.ts
+++ pages/api/product/index.ts
@@ -3,13 +3,17 @@ import {
   createProduct,
   getAllProduct,
 } from "@/backend/controllers/productController";
+import validateBody from "@/backend/middleware/validateBody";
 import createRouter from "next-connect";
 
 dbConnect(process.env.MONGO_URI as string);
 
 const router = createRouter();
 
-router.post(createProduct);
+router.post(
+  validateBody(["name", "description", "price", "stock", "seller", "category"]),
+  createProduct
+);
 router.get(getAllProduct);
 
 export default router;
