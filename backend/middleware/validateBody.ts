09255e675f40b5e7b4b1a3c944d1ee5b5b13e0c2
al-imam <alimam01828@gmail.com>
2023-05-22 02:33:11 +0600

    change validate HOC architecture handle string case

--- backend/middleware/validateBody.ts
+++ backend/middleware/validateBody.ts
@@ -2,21 +2,32 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextHandler } from "next-connect";
 import wrap from "@/utility/wrapHandler";
 
-function validateBody(properties: string[]) {
+type ValidateFunction = (value: any) => boolean;
+
+interface ObjectValidate {
+  property: string;
+  validate: ValidateFunction;
+}
+
+type ItemType = string | [string, ValidateFunction] | ObjectValidate;
+
+function validateBody(items: ItemType[]) {
   return wrap(
     async (req: NextApiRequest, res: NextApiResponse, next: NextHandler) => {
-      const missingItems: string[] = [];
+      const missingItems: any[] = [];
 
-      properties.forEach((property) => {
-        if (!req.body.hasOwnProperty(property)) {
-          missingItems.push(property);
+      items.forEach((item) => {
+        if (typeof item === "string") {
+          if (!req.body.hasOwnProperty(item)) {
+            missingItems.push(item);
+          }
         }
       });
 
       if (missingItems.length === 0) return next();
 
       return res.status(400).json({
-        message: "some properties are missing in request body",
+        message: "some properties are missing or not valid in request body",
         "missing-property": missingItems,
       });
     },
