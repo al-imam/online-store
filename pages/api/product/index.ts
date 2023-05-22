84777c1d91f2ee10fc5fc8124fcb6943e4dc8cbe
al-imam <alimam01828@gmail.com>
2023-05-22 15:30:32 +0600

    validate optional property

--- pages/api/product/index.ts
+++ pages/api/product/index.ts
@@ -11,6 +11,11 @@ dbConnect(process.env.MONGO_URI as string);
 
 const router = createRouter();
 
+function validUndefined(value: any, callback: () => boolean) {
+  if (value === undefined) return true;
+  return callback();
+}
+
 router.post(
   validateBody([
     ["name", (name) => typeof name === "string"],
@@ -23,6 +28,23 @@ router.post(
       (category) => categories.map((i) => i.toLowerCase()).includes(category),
     ],
   ]),
+  validateBody(
+    [
+      [
+        "rating",
+        (rating) => validUndefined(rating, () => typeof rating === "number"),
+      ],
+      [
+        "images",
+        (images) => validUndefined(images, () => Array.isArray(images)),
+      ],
+      [
+        "reviews",
+        (reviews) => validUndefined(reviews, () => Array.isArray(reviews)),
+      ],
+    ],
+    { strict: false }
+  ),
   createProduct
 );
 router.get(getAllProduct);
