00314f3353bde5ddca918e431fab190e82246c59
al-imam <alimam01828@gmail.com>
2023-05-22 18:20:25 +0600

    validate object id and test endpoints

--- pages/api/product/[id].ts
+++ pages/api/product/[id].ts
@@ -1,8 +1,9 @@
 import { getProduct } from "@/backend/controllers/productController";
 import createRouter from "next-connect";
+import validateObjectId from "@/backend/middleware/validateObjectId";
 
 const router = createRouter();
 
-router.get(getProduct);
+router.get(validateObjectId(["id"], "query"), getProduct);
 
 export default router;
