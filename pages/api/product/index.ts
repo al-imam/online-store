1750495ce9b5246513ca14b4b20bcd36b1b97808
al-imam <alimam01828@gmail.com>
2023-05-22 15:08:37 +0600

    validate required body

--- pages/api/product/index.ts
+++ pages/api/product/index.ts
@@ -5,13 +5,24 @@ import {
 } from "@/backend/controllers/productController";
 import validateBody from "@/backend/middleware/validateBody";
 import createRouter from "next-connect";
+import categories from "@/utility/categories";
 
 dbConnect(process.env.MONGO_URI as string);
 
 const router = createRouter();
 
 router.post(
-  validateBody(["name", "description", "price", "stock", "seller", "category"]),
+  validateBody([
+    ["name", (name) => typeof name === "string"],
+    ["description", (description) => typeof description === "string"],
+    ["price", (price) => typeof price === "number"],
+    ["stock", (stock) => typeof stock === "number"],
+    ["seller", (seller) => typeof seller === "string"],
+    [
+      "category",
+      (category) => categories.map((i) => i.toLowerCase()).includes(category),
+    ],
+  ]),
   createProduct
 );
 router.get(getAllProduct);
