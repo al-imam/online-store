18aad18d472c57d7c50aae4fed5bd18dfefac0ae
al-imam <alimam01828@gmail.com>
2023-05-24 00:49:47 +0600

    add url regex from https://regexr.com/39nr7

--- utility/regex.ts
+++ utility/regex.ts
@@ -58,3 +58,5 @@ const emailRegex = new RegExp(
 );
 
 export default emailRegex;
+export const urlRegex =
+  /^[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
