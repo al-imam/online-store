712ee143c2b4c784a02023daa2c9454dd46bec2f
al-imam <alimam01828@gmail.com>
2023-05-23 23:15:11 +0600

    add basic validation to required field

--- pages/api/auth/singup.ts
+++ pages/api/auth/singup.ts
@@ -1,8 +1,16 @@
 import createRouter from "next-connect";
+import validateBody from "@/backend/middleware/validateBody";
 import { singup } from "@/backend/controllers/authController";
 
 const router = createRouter();
 
-router.post(singup);
+router.post(
+  validateBody([
+    ["name", (name) => typeof name === "string" && name.length < 30],
+    ["email", (email) => typeof email === "string"],
+    ["password", (password) => ["string", "number"].includes(typeof password)],
+  ]),
+  singup
+);
 
 export default router;
