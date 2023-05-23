236f2ae1ab1cedca3412a2b803f1c28a128ccc25
al-imam <alimam01828@gmail.com>
2023-05-23 23:45:27 +0600

    validate email with regex

--- pages/api/auth/singup.ts
+++ pages/api/auth/singup.ts
@@ -1,13 +1,20 @@
 import createRouter from "next-connect";
 import validateBody from "@/backend/middleware/validateBody";
 import { singup } from "@/backend/controllers/authController";
+import dbConnect from "@/backend/config/dbConnect";
+import emailRegex from "@/utility/regex";
+
+dbConnect();
 
 const router = createRouter();
 
 router.post(
   validateBody([
     ["name", (name) => typeof name === "string" && name.length < 30],
-    ["email", (email) => typeof email === "string"],
+    [
+      "email",
+      (email) => typeof email === "string" && email.match(emailRegex) !== null,
+    ],
     ["password", (password) => ["string", "number"].includes(typeof password)],
   ]),
   singup
