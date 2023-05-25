5a9e47e2d32e13c041bdab85025e0a6d03364abe
al-imam <alimam01828@gmail.com>
2023-05-25 21:49:30 +0600

    add toast provider and css

--- context/GlobalProvider.tsx
+++ context/GlobalProvider.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { AuthProvider } from "@/context/AuthProvider";
+import { ToastContainer } from "react-toastify";
 
 function GlobalProvider({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <ToastContainer position="top-center" />
+      {children}
+    </AuthProvider>
+  );
 }
 
 export default GlobalProvider;
