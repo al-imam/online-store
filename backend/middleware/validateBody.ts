55ae191bd85509ccb5a8ed6f58bf0758e5307b8c
al-imam <alimam01828@gmail.com>
2023-05-25 23:51:04 +0600

    remove my cstm code do validation with my package

--- backend/middleware/validateBody.ts
+++ backend/middleware/validateBody.ts
@@ -1,15 +1,8 @@
 import wrap from "@/utility/wrapHandler";
+import validate from "nested-object-validate";
+import { ValidatorType } from "nested-object-validate/dist/types";
 
-type ValidateFunction = (value: any) => boolean;
-
-interface ObjectValidate {
-  property: string;
-  validate: ValidateFunction;
-}
-
-type ItemType = string | [string, ValidateFunction] | ObjectValidate;
-
-function validateBody(validators: ItemType[], config = { strict: true }) {
+function validateBody(validators: ValidatorType[], config = { strict: true }) {
   return wrap(async (req, res, next) => {
     if (!(req.body instanceof Object && !Array.isArray(req.body))) {
       return res.status(400).json({
@@ -18,70 +11,30 @@ function validateBody(validators: ItemType[], config = { strict: true }) {
       });
     }
 
-    const missings: string[] = [];
-    const invalids: string[] = [];
-    const valids: { [key: string]: any } = {};
+    const result = validate(req.body, validators);
 
-    for (const validator of validators) {
-      if (typeof validator === "string") {
-        if (req.body.hasOwnProperty(validator)) {
-          valids[validator] = req.body[validator];
-          continue;
-        }
-
-        missings.push(validator);
-      }
-
-      if (Array.isArray(validator)) {
-        if (!req.body.hasOwnProperty(validator[0]) && config.strict) {
-          missings.push(validator[0]);
-          continue;
-        }
-
-        if (!validator[1](req.body[validator[0]])) {
-          invalids.push(validator[0]);
-          continue;
-        }
-
-        if (req.body[validator[0]] === undefined) continue;
-        valids[validator[0]] = req.body[validator[0]];
-      }
-
-      if (validator instanceof Object && !Array.isArray(validator)) {
-        if (!req.body.hasOwnProperty(validator.property) && config.strict) {
-          missings.push(validator.property);
-          continue;
-        }
-
-        if (!validator.validate(req.body[validator.property])) {
-          invalids.push(validator.property);
-          continue;
-        }
-
-        if (req.body[validator.property] === undefined) continue;
-        valids[validator.property] = req.body[validator.property];
-      }
-    }
-
-    if (missings.length === 0 && invalids.length === 0) {
+    if (result.valid) {
       if (
         req.body._valid_object instanceof Object &&
         !Array.isArray(req.body._valid_object)
       ) {
-        req.body._valid_object = Object.assign(req.body._valid_object, valids);
+        req.body._valid_object = Object.assign(
+          req.body._valid_object,
+          result.checked
+        );
       }
 
       if (req.body._valid_object === undefined) {
-        req.body._valid_object = valids;
+        req.body._valid_object = result.checked;
       }
 
       return next();
     }
 
     return res.status(400).json({
-      message: "some properties are missing or invalid in request body",
-      "missing-properties": missings,
-      "invalid-properties": invalids,
+      message: "some properties are missing or invalid",
+      missing: result.missing,
+      invalid: result.invalid,
     });
   }, "validate-body");
 }
