1bd27c968e4a741dbc1e12243cacb6c57d750b6b
al-imam <alimam01828@gmail.com>
2023-05-26 15:07:41 +0600

    make form Input reuseable

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -1,30 +1,21 @@
 "use client";
 
 import type { FunctionComponent, InputHTMLAttributes } from "react";
-import useSingup from "@/store/useSingup";
 
 interface InputProps {
   text: string;
-  name: "name" | "password" | "email";
+  setValue: (value: string) => void;
 }
 
 const Input: FunctionComponent<
-  InputProps &
-    Omit<
-      InputHTMLAttributes<HTMLInputElement>,
-      "className" | "name" | "value" | "onChange"
-    >
-> = ({ text, name, ...rest }) => {
-  const value = useSingup((store) => store[name]);
-  const setValue = useSingup((store) => store[`${name}Set`]);
-
+  InputProps & Omit<InputHTMLAttributes<HTMLInputElement>, "className">
+> = ({ text, setValue, ...rest }) => {
   return (
     <div className="mb-4">
       <label className="block mb-1"> {text} </label>
       <input
         className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
         {...rest}
-        value={value}
         onChange={(e) => setValue(e.target.value)}
       />
     </div>
