50f8c402dd0e11aecdb368ccd697c5e4410475de
al-imam <alimam01828@gmail.com>
2023-05-27 20:15:05 +0600

    create new address component

--- components/auth/Me.tsx
+++ components/auth/Me.tsx
@@ -21,15 +21,15 @@ export default function () {
         <figcaption>
           <h5 className="text-gray-950 text-lg">{currentUser?.name}</h5>
           <div>
-            <span>Email: {currentUser.email}</span>
-            <span>Joined On: {time.format(new Date(currentUser.created))}</span>
+            <p>Email: {currentUser.email}</p>
+            <p>Joined On: {time.format(new Date(currentUser.created))}</p>
           </div>
         </figcaption>
       </figure>
 
       <hr className="my-4" />
 
-      <Link href="/address/new">
+      <Link href="/me/address/new">
         <button className="px-4 py-2 inline-block text-blue-600 border border-gray-300 rounded-md hover:bg-gray-100">
           <i className="mr-1 fa fa-plus"></i> Add new address
         </button>
