01f098a7cbbddf114c92528a96a0a806b92571f7
al-imam <alimam01828@gmail.com>
2023-05-27 23:09:40 +0600

    validate req country property

--- pages/api/address/index.ts
+++ pages/api/address/index.ts
@@ -1,11 +1,26 @@
 import createRouter from "next-connect";
 import dbConnect from "@/backend/config/dbConnect";
 import { addAddress } from "@/backend/controllers/addressController";
+import validateBody from "@/backend/middleware/validateBody";
+import { countries } from "countries-list";
 
 const router = createRouter();
 
+const countrys = Object.values(countries);
+
 dbConnect();
 
-router.post(addAddress);
+router.post(
+  validateBody([
+    [
+      "country",
+      (country) =>
+        countrys.find((c) => c.name.toLowerCase() === country?.toLowerCase())
+          ? true
+          : `${country} is not valid country`,
+    ],
+  ]),
+  addAddress
+);
 
 export default router;
