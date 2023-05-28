b44860d2e21155ece6afe81a4b25f930d83cc174
al-imam <alimam01828@gmail.com>
2023-05-28 23:03:06 +0600

    compare password with bcryptjs

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,5 +1,6 @@
 import User from "@/backend/models/user";
 import wrap from "@/utility/wrapHandler";
+import { compareSync } from "bcryptjs";
 
 const singup = wrap(async (req, res) => {
   const user = await User.create(req.body._valid_object);
@@ -7,12 +8,19 @@ const singup = wrap(async (req, res) => {
 }, "singup");
 
 const singin = wrap(async (req, res) => {
-  const user = await User.findById(req.body.userId);
+  const user = await User.findOne({ email: req.body.email });
 
   if (user === null) {
     return res.status(400).json({
       code: "singin-failed",
       message: "user not found",
+    });
+  }
+
+  if (!compareSync(user.password, req.body.password)) {
+    return res.status(400).json({
+      code: "singin-failed",
+      message: "email and password not match",
     });
   }
 
