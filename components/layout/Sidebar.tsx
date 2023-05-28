2a6bac029b1e9700166b897959b805fbe3210472
al-imam <alimam01828@gmail.com>
2023-05-29 01:06:50 +0600

    implement singout and redirect

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import useAuth from "@/context/AuthProvider";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function () {
+  const { singout } = useAuth();
+  const router = useRouter();
+
   return (
     <aside className="md:w-1/3 lg:w-1/4 px-4">
       <ul className="sidebar">
@@ -80,7 +85,7 @@ export default function () {
         <li>
           <a
             className="block px-3 py-2 text-red-800 hover:bg-red-100 hover:text-white-500 rounded-md cursor-pointer"
-            onClick={() => {}}
+            onClick={() => singout(() => router.push("/"))}
           >
             Logout
           </a>
