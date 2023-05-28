2c443e0b2cc37d9c50ec66cd8b2e52a5b75337df
al-imam <alimam01828@gmail.com>
2023-05-28 23:21:19 +0600

    validate singin req body

--- pages/api/auth/singin.ts
+++ pages/api/auth/singin.ts
@@ -1,11 +1,29 @@
 import dbConnect from "@/backend/config/dbConnect";
 import { singin } from "@/backend/controllers/authController";
 import createRouter from "next-connect";
+import validateBody from "@/backend/middleware/validateBody";
+import emailRegex from "@/utility/regex";
 
 dbConnect();
 
 const router = createRouter();
 
-router.post(singin);
+router.post(
+  validateBody([
+    [
+      "email",
+      (email) =>
+        (typeof email === "string" && email.match(emailRegex) !== null) ||
+        "email is not valid",
+    ],
+    [
+      "password",
+      (password) =>
+        (typeof password === "string" && password.length >= 6) ||
+        "password must've string and 6 character or upper",
+    ],
+  ]),
+  singin
+);
 
 export default router;
