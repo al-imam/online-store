eaff25347479731375aac5a1ee54e0738e47b7b6
al-imam <alimam01828@gmail.com>
2023-05-28 22:50:20 +0600

    create singin method

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -6,6 +6,19 @@ const singup = wrap(async (req, res) => {
   res.status(201).json(remove(user));
 }, "singup");
 
+const singin = wrap(async (req, res) => {
+  const user = await User.findById(req.body.userId);
+
+  if (user === null) {
+    return res.status(400).json({
+      code: "singin-failed",
+      message: "user not found",
+    });
+  }
+
+  res.status(201).json(remove(user));
+}, "singin");
+
 function remove({
   name,
   email,
