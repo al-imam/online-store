b3f63ecd020695a8fd8b2942668e57bdc21bad34
al-imam <alimam01828@gmail.com>
2023-05-29 03:51:15 +0600

    optimize authGuard code

--- backend/middleware/AuthGuard.ts
+++ backend/middleware/AuthGuard.ts
@@ -1,23 +1,18 @@
 import verifyJWTToken from "../util/VerifyJWTToken";
 import wrap from "@/utility/wrapHandler";
+import { isValidObjectId } from "mongoose";
 
 export default wrap(async ({ headers: { authorization }, body }, res, next) => {
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(401).json({
-      code: "authorization",
-      message: "authorization headers is not valid or not sent",
-    });
-  }
-
-  const { id } = verifyJWTToken(authorization.replace("Bearer ", ""));
-
-  if (id !== null) {
-    body._id = id;
-    return next();
+  if (authorization && authorization.startsWith("Bearer ")) {
+    const { id } = verifyJWTToken(authorization.replace("Bearer ", ""));
+    if (id !== null && isValidObjectId(id)) {
+      body._id = id;
+      return next();
+    }
   }
 
   return res.status(401).json({
     code: "authorization",
-    message: "authorization headers is not valid",
+    message: "authorization headers is not valid or not sent",
   });
 }, "authorization");
