43868b0654db6f39087128460e23acf17ebaf138
al-imam <alimam01828@gmail.com>
2023-05-30 01:17:47 +0600

    get jwt token from cookies and set as auth token

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,9 +1,19 @@
 import Me from "@/components/auth/Me";
 import { Get } from "@/utility/request";
 import AddressInterface from "@/types/AddressInterface";
+import { cookies } from "next/headers";
 
 export default async function () {
-  const { data } = await Get<(AddressInterface & { _id: string })[]>("address");
+  const jwt = cookies().get("jsonwebtoken")?.value;
+
+  const { data } = await Get<(AddressInterface & { _id: string })[]>(
+    "address",
+    {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    }
+  );
 
   return <Me addresses={data} />;
 }
