022f17e43c7853643f72bd4451b28442bc2a09d9
al-imam <alimam01828@gmail.com>
2023-05-30 21:06:08 +0600

    modify validate ObjectId HOC

--- backend/middleware/validateObjectId.ts
+++ backend/middleware/validateObjectId.ts
@@ -4,19 +4,35 @@ import wrap from "@/utility/wrapHandler";
 function validateObjectId(ids: string[], validate: "body" | "query" = "body") {
   return wrap(async (req, res, next) => {
     const invalidIds: Record<string, string>[] = [];
+    const valid: Record<string, string> = {};
 
     ids.forEach((id) => {
       if (!Types.ObjectId.isValid(req[validate][id])) {
         invalidIds.push({ [id]: req[validate][id] });
+      } else {
+        valid[id] = req[validate][id];
       }
     });
 
-    if (invalidIds.length === 0) return next();
+    if (invalidIds.length === 0) {
+      if (
+        req.body.VALID_REQ instanceof Object &&
+        !Array.isArray(req.body.VALID_REQ)
+      ) {
+        req.body.VALID_REQ = Object.assign(req.body.VALID_REQ, valid);
+      }
+
+      if (req.body.VALID_REQ === undefined) {
+        req.body.VALID_REQ = valid;
+      }
+
+      return next();
+    }
 
     return res.status(400).json({
       code: "invalid-object-id",
       message: "some object id not valid",
-      "invalid-object-ids": invalidIds,
+      invalid: invalidIds,
     });
   }, "validate-object-id");
 }
