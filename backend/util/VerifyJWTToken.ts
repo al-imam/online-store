0268a4b2d7eaab68baf2011fa9702f69505b86a2
al-imam <alimam01828@gmail.com>
2023-05-30 08:13:58 +0600

    implement verify unction with jose

--- backend/util/VerifyJWTToken.ts
+++ backend/util/VerifyJWTToken.ts
@@ -1,9 +1,22 @@
-import { verify } from "jsonwebtoken";
+import { verify as dsf } from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
 function verifyJWTToken(jwtToken: string): { id: string | null } {
   try {
-    return verify(jwtToken, process.env.JWT_SECRET || "") as { id: string };
+    return dsf(jwtToken, process.env.JWT_SECRET || "") as { id: string };
   } catch {
+    return { id: null };
+  }
+}
+
+export async function verify(token: string) {
+  try {
+    const verified = await jwtVerify(
+      token,
+      new TextEncoder().encode(process.env.JWT_SECRET)
+    );
+    return verified.payload as { id: string };
+  } catch (err) {
     return { id: null };
   }
 }
