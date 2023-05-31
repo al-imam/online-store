caadea74b53b1aa026e0a210451fa8d99e95387e
al-imam <alimam01828@gmail.com>
2023-05-31 21:18:50 +0600

    format save file name

--- backend/util/multer.ts
+++ backend/util/multer.ts
@@ -1,12 +1,37 @@
 import multer, { diskStorage } from "multer";
 
+const time = new Intl.DateTimeFormat("en", {
+  day: "2-digit",
+  second: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  minute: "2-digit",
+  hour: "2-digit",
+});
+
+function getTime() {
+  return time
+    .format(new Date())
+    .slice(0, -3)
+    .replaceAll("/", "-")
+    .replaceAll(":", "-")
+    .replaceAll(", ", "-");
+}
+
 export default multer({
   storage: diskStorage({
     destination: function (req, file, callback) {
       callback(null, "public/saves");
     },
-    filename: function (req, file, callback) {
-      callback(null, `{${new Date().toDateString()}-${file.originalname}`);
+    filename: function (req, { originalname }, callback) {
+      callback(
+        null,
+        `${originalname
+          .trim()
+          .toLowerCase()
+          .replaceAll(" ", "")
+          .replaceAll("\n", "")}-${getTime()}`
+      );
     },
   }),
 
