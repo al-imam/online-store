348c4ace062a160a08eb199b89040b8381b47812
al-imam <alimam01828@gmail.com>
2023-05-31 20:44:03 +0600

    install and setup cloudinary

---
+++ backend/util/cloudinary.ts
@@ -0,0 +1,36 @@
+import { v2 } from "cloudinary";
+
+v2.config({
+  cloud_name: process.env.SETUP_PLEASE,
+  api_key: process.env.SETUP_PLEASE,
+  api_secret: process.env.SETUP_PLEASE,
+});
+
+function save(
+  file: string,
+  folder: string
+): Promise<{ id: string; url: string }> {
+  return new Promise((resolve, reject) => {
+    v2.uploader.upload(
+      file,
+
+      {
+        resource_type: "auto",
+        folder: folder,
+      },
+
+      (err, result) => {
+        if (result !== undefined) {
+          return resolve({
+            id: result.public_id,
+            url: result.url,
+          });
+        }
+
+        reject(err);
+      }
+    );
+  });
+}
+
+export { save };
