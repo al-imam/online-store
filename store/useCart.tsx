efeb4a2539fef7a52692da25b6223f2b21b6cb60
al-imam <alimam01828@gmail.com>
2023-06-03 14:16:08 +0600

    modify store add middleware and return copy state

--- store/useCart.tsx
+++ store/useCart.tsx
@@ -1,67 +1,79 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, subscribeWithSelector } from "zustand/middleware";
 import CartItemInterface from "@/types/cartItemInterface";
 
 interface StoreInterface {
   items: CartItemInterface[];
+  unit: number;
+  total: number;
   addItem(item: CartItemInterface): void;
   addQuantity(item: CartItemInterface): void;
   removeQuantity(item: CartItemInterface): void;
   deleteItem(id: string): void;
 }
 
-const useCart = create<StoreInterface, [["zustand/persist", StoreInterface]]>(
+const useCart = create<
+  StoreInterface,
+  [
+    ["zustand/persist", StoreInterface],
+    ["zustand/subscribeWithSelector", never]
+  ]
+>(
   persist(
-    (set, get) => ({
+    subscribeWithSelector((set, get) => ({
       items: [],
+      unit: 0,
+      total: 0,
 
       addItem(item) {
         const { items, addQuantity } = get();
 
         const update = items.find((e) => e.id === item.id);
-        if (update) return addQuantity(update);
 
-        set({ items: [...items, item] });
+        if (update) {
+          addQuantity(update);
+        } else {
+          set((store) => ({ ...store, items: [...items, item] }));
+        }
       },
 
       deleteItem(id) {
         set((store) => ({
+          ...store,
           items: store.items.filter((item) => item.id !== id),
         }));
       },
 
       addQuantity(item) {
-        const { items } = get();
-
         if (item && item.stock > item.quantity) {
-          set({
-            items: items.map((el) => {
+          set((store) => ({
+            ...store,
+            items: store.items.map((el) => {
               if (el.id === item.id) {
                 el.quantity++;
                 return el;
               }
               return el;
             }),
-          });
+          }));
         }
       },
 
       removeQuantity(item) {
-        const { items } = get();
-
         if (item.quantity > 1) {
-          set({
-            items: items.map((el) => {
+          set((store) => ({
+            ...store,
+            items: store.items.map((el) => {
               if (el.id === item.id) {
                 el.quantity--;
                 return el;
               }
               return el;
             }),
-          });
+          }));
         }
       },
-    }),
+    })),
     { name: "cart-items", skipHydration: true }
   )
 );
