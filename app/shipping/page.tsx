12a5634acca06989c6f0ed8595f6e61b74352124
al-imam <alimam01828@gmail.com>
2023-06-04 09:05:56 +0600

    fetch addresses

--- app/shipping/page.tsx
+++ app/shipping/page.tsx
@@ -1,4 +1,21 @@
 import Shipping from "@/components/cart/Shipping";
+import { Get } from "@/utility/request";
+import { cookies } from "next/headers";
+import COOKIES from "@/utility/COOKIES";
+import AddressInterface from "@/types/AddressInterface";
+import Prettify from "@/types/Prettify";
+
+export default async function () {
+  const jwt = cookies().get(COOKIES)?.value;
+
+  const { data } = await Get<Prettify<AddressInterface & { _id: string }>[]>(
+    "address",
+    {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    }
+  );
 
 export default function () {
   return <Shipping />;
