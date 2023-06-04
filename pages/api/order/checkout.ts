98fae528e3c104bc83789cc18cb08c123eae0599
al-imam <alimam01828@gmail.com>
2023-06-05 03:02:22 +0600

    validate checkout post body and test

--- pages/api/order/checkout.ts
+++ pages/api/order/checkout.ts
@@ -1,11 +1,51 @@
 import dbConnect from "@/backend/config/dbConnect";
 import { checkout } from "@/backend/controllers/orderController";
+import validateBody from "@/backend/middleware/validateBody";
+import Prettify from "@/types/Prettify";
+import CartItemInterface from "@/types/cartItemInterface";
+import { urlRegex } from "@/utility/regex";
+
 import createRouter from "next-connect";
+
+type C = Prettify<CartItemInterface>;
 
 dbConnect();
 
 const router = createRouter();
 
-router.post(checkout);
+router.post(
+  validateBody([
+    {
+      property: "items",
+      validate: (items, validate) => {
+        if (!Array.isArray(items)) return "required array of items";
+
+        if (
+          items.every((item) =>
+            validate(item, [
+              [
+                "quantity",
+                (quantity) => typeof quantity === "number" && quantity > 0,
+              ],
+              ["price", (price) => typeof price === "number" && price > 0],
+              ["name", (name) => typeof name === "string"],
+              [
+                "picture",
+                (picture) =>
+                  (typeof picture === "string" && urlRegex.test(picture)) ||
+                  picture === null,
+              ],
+            ])
+          )
+        ) {
+          return true;
+        }
+
+        return "items array doesn't contain valid item";
+      },
+    },
+  ]),
+  checkout
+);
 
 export default router;
