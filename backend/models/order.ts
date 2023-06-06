8bc4980775e24846e5490dad9af34017fc40e4a7
al-imam <alimam01828@gmail.com>
2023-06-06 16:37:51 +0600

    create order schema and model

--- backend/models/order.ts
+++ backend/models/order.ts
@@ -1,22 +1,89 @@
 import { Model, Schema, Types, model, models } from "mongoose";
 import Prettify from "@/types/Prettify";
 
+interface Payment {
+  id: string;
+  status: string;
+  tax: number;
+  amount: number;
+}
+
+interface Order {
+  product: Schema.Types.ObjectId;
+  name: string;
+  quantity: number;
+  imageURL: string;
+  price: number;
+}
+
 interface OrderSchema {
   address: Schema.Types.ObjectId;
   user: Schema.Types.ObjectId;
-  order: {
-    product: Schema.Types.ObjectId;
-    name: string;
-    quantity: number;
-    imageURL: string;
-    price: number;
-  }[];
-  payment: {
-    id: string;
-    status: string;
-    tax: number;
-    amount: number;
-  };
+  order: Order[];
+  payment: Payment;
   status: "processing" | "pending" | "completed";
   created: Date;
 }
+
+const order = new Schema<Prettify<OrderSchema>>(
+  {
+    address: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "Address",
+    },
+
+    user: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    order: [
+      {
+        product: {
+          type: Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+
+        name: {
+          type: String,
+          required: true,
+        },
+
+        quantity: {
+          type: String,
+          required: true,
+        },
+
+        imageURL: {
+          type: String,
+          required: true,
+        },
+
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+
+    payment: {
+      id: { type: String, required: true },
+      status: { type: String, required: true },
+      tax: { type: Number, required: true },
+      amount: { type: Number, required: true },
+    },
+
+    status: {
+      type: String,
+      enum: ["processing", "pending", "completed"] as const,
+      default: "processing",
+    },
+  },
+  { timestamps: { updatedAt: false, createdAt: "created" } }
+);
+
+export default (models.Order as Model<Prettify<OrderSchema>>) ||
+  model("Order", order);
