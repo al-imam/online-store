8ab5fa390a9b64d352d0e8c34f754e76f9757b83
al-imam <alimam01828@gmail.com>
2023-06-08 06:19:37 +0600

    create order page

---
+++ components/order/Order.tsx
@@ -0,0 +1,20 @@
+import Pagination from "@/components/layout/Pagination";
+import OrderItem from "@/components/order/OrderItem";
+import { OrderResponse } from "@/types/OrderInterface";
+
+interface OderProps {
+  data: OrderResponse;
+}
+
+export default function ({ data }: OderProps) {
+  return (
+    <>
+      <h3 className="text-xl font-semibold mb-5">Your Orders</h3>
+      {data.orders.map((order) => (
+        <OrderItem key={order._id} order={order} />
+      ))}
+
+      <Pagination single={data.single} total={data.count} />
+    </>
+  );
+}
