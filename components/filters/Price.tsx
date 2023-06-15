5543559b988e439001234f2c91f88680598c2a67
al-imam <alimam01828@gmail.com>
2023-06-16 03:09:38 +0600

    implement price filtering

--- components/filters/Price.tsx
+++ components/filters/Price.tsx
@@ -1,4 +1,71 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, ChangeEvent } from "react";
+import useObjectStore from "use-object-store";
+
+const num = /(^\d*\.?\d*[0-9]+\d*$)|(^[0-9]+\d*\.\d*$)/;
+
 export default function () {
+  const [{ min, max }, dispatch] = useObjectStore({ min: "", max: "" });
+  const router = useRouter();
+
+  useEffect(() => {
+    const qp = new URLSearchParams(window.location.search);
+
+    if (qp.get("min") !== null) {
+      const min = parseFloat(qp.get("min") as any);
+      if (!isNaN(min)) dispatch({ min: min.toString() });
+    }
+
+    if (qp.get("max") !== null) {
+      const max = parseFloat(qp.get("max") as any);
+      if (!isNaN(max)) dispatch({ max: max.toString() });
+    }
+  }, []);
+
+  function changePriceQuery(min: string, max: string) {
+    const qp = new URLSearchParams(window.location.search);
+
+    const p = (name: string) => parseInt(qp.get("min")!) || "";
+    const changePage = min !== p("min") || max !== p("max");
+
+    if (min) {
+      qp.set("min", min);
+    } else {
+      qp.delete("min");
+    }
+
+    if (max) {
+      qp.set("max", max);
+    } else {
+      qp.delete("max");
+    }
+
+    if (changePage) {
+      qp.delete("page");
+      qp.set("page", "1");
+    }
+
+    router.push(`/?${qp.toString()}`);
+  }
+
+  function reset() {
+    dispatch({ min: "", max: "" });
+    const qp = new URLSearchParams(window.location.search);
+    qp.delete("min");
+    qp.delete("max");
+    router.push(`/?${qp.toString()}`);
+  }
+
+  function validateNumber(callback: (value: string) => void) {
+    return (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.value === "" || num.test(e.target.value)) {
+        callback(e.target.value);
+      }
+    };
+  }
+
   return (
     <details className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
       <summary className="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
@@ -30,6 +97,7 @@ export default function () {
 
           <button
             type="button"
+            onClick={reset}
             className="text-sm text-gray-900 underline underline-offset-4"
           >
             Reset
@@ -49,6 +117,11 @@ export default function () {
                 id="filter-price-from"
                 placeholder="From"
                 className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm form-input"
+                value={min}
+                onChange={validateNumber((value) => {
+                  dispatch({ min: value });
+                  changePriceQuery(value, max);
+                })}
               />
             </label>
 
@@ -63,6 +136,11 @@ export default function () {
                 id="filter-price-to"
                 placeholder="To"
                 className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm form-input"
+                value={max}
+                onChange={validateNumber((value) => {
+                  dispatch({ max: value });
+                  changePriceQuery(min, value);
+                })}
               />
             </label>
           </div>
