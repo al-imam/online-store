5964a5bc1ea3739e2554f6eb6c4f552f99e45c51
al-imam <alimam01828@gmail.com>
2023-06-19 01:31:15 +0600

    implement add item action to add items

--- store/productSlice.ts
+++ store/productSlice.ts
@@ -10,5 +10,18 @@ export const productSlice = createSlice({
     clear: function (state) {
       state.items = [];
     },
+
+    addItem: function (state, action: PayloadAction<CartItemInterface>) {
+      const index = state.items.findIndex((e) => e.id === action.payload.id);
+
+      if (index === -1) {
+        state.items.push(action.payload);
+        return;
+      }
+
+      if (state.items[index].stock > state.items[index].quantity) {
+        state.items[index].quantity++;
+      }
+    },
   },
 });
