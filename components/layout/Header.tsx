60ba92ddd69a4fecbdd1a2390f1eaff644f7006f
al-imam <alimam01828@gmail.com>
2023-06-22 02:45:09 +0600

    make navbar better responsive

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,32 +1,16 @@
+"use client";
+
 import Search from "$components/layout/Search";
-import HeaderItems from "$components/utility/HeaderItems";
+import { Auth } from "$components/utility";
+import { useSelector } from "$store/index";
 import Link from "next/link";
 
-const Header = () => (
-  <header className="bg-white flex h-[var(--nav-size)] items-center border-b ">
-    <div className="container max-w-screen-xl mx-auto px-4">
-      <div className="flex flex-wrap items-center">
-        <div className="flex-shrink-0 mr-5">
-          <Link href="/">
-            <span className="text-gray-700 text-xl">store</span>
-          </Link>
-        </div>
-        <Search />
+export default function () {
+  const count = useSelector((state) => state.items.length);
 
-        <HeaderItems />
-
-        <div className="lg:hidden ml-2">
-          <button
-            type="button"
-            className="bg-white p-3 inline-flex items-center rounded-md text-black hover:bg-gray-200 hover:text-gray-800 border border-transparent"
-          >
-            <span className="sr-only">Open menu</span>
-            <i className="fa fa-bars fa-lg"></i>
-          </button>
-        </div>
-      </div>
-    </div>
-  </header>
-);
-
-export default Header;
+  return (
+    <header className="bg-white text-gray-800 flex h-16 items-center shadow-sm">
+      <nav className="mx-auto max-w-screen-xl px-4 flex gap-4 items-center relative w-full"></nav>
+    </header>
+  );
+}
