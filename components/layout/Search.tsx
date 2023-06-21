ebe0baea178dd8d8d11e150e5e63d9f80de12f86
al-imam <alimam01828@gmail.com>
2023-06-22 02:43:37 +0600

    change search input look and feel

--- components/layout/Search.tsx
+++ components/layout/Search.tsx
@@ -34,20 +34,35 @@ export default function () {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-nowrap items-center w-full order-last md:order-none mt-5 md:mt-0 md:w-2/4 lg:w-2/4"
-    >
+    <form className="relative w-full md:w-2/4 mr-auto" onSubmit={handleSubmit}>
+      <label className="sr-only">Search</label>
+
       <input
-        className="flex-grow appearance-none border border-gray-200 bg-gray-100 rounded-md mr-2 py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400"
         type="text"
         placeholder="Enter your keyword"
+        className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm form-input"
         value={search}
         onChange={searchChange}
       />
-      <button className="px-4 py-2 inline-block text-white border border-transparent bg-blue-600 rounded-md hover:bg-blue-700">
-        Search
-      </button>
+
+      <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+        <button className="text-gray-600 hover:text-gray-700 focus-visible:text-blue-400 focus-visible:outline-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-4 w-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </span>
     </form>
   );
 }
