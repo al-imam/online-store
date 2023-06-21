7b49d49dd9eca6752f0796f572fdfe9d3df78834
al-imam <alimam01828@gmail.com>
2023-06-21 08:33:49 +0600

    create helper component for profile model

--- components/nav/Profile.tsx
+++ components/nav/Profile.tsx
@@ -1,5 +1,27 @@
 import Link from "next/link";
 
+interface ItemProps {
+  close: () => void;
+  icon: React.ReactNode;
+  action: string | (() => void);
+  text: string;
+  className?: string;
+}
+
+function Item({ close, icon, action, text, className }: ItemProps) {
+  return (
+    <li onClick={close} className={className}>
+      <ButtonOrA
+        action={action}
+        className="px-3 py-3 w-full text-sm font-medium flex items-center space-x-2 hover:bg-blue-100/70 focus-visible:outline-none focus-visible:bg-blue-100/70"
+      >
+        {icon}
+        <span> {text} </span>
+      </ButtonOrA>
+    </li>
+  );
+}
+
 function ButtonOrA({
   className,
   action,
