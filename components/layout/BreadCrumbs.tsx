826895381b2cec302514d73e36bcc3a75835f11e
al-imam <alimam01828@gmail.com>
2023-06-23 19:36:47 +0600

    change bread crumbs

--- components/layout/BreadCrumbs.tsx
+++ components/layout/BreadCrumbs.tsx
@@ -1,3 +1,4 @@
+import { ArrowRightIcon, HomeIcon } from "$svg/icons";
 import Link from "next/link";
 
 interface BreadCrumbsProps {
@@ -7,24 +8,27 @@ interface BreadCrumbsProps {
   }[];
 }
 
-const BreadCrumbs = ({ list }: BreadCrumbsProps) => (
-  <section className="py-5 sm:py-7 bg-blue-100">
-    <div className="container max-w-screen-xl mx-auto px-4">
-      <ol className="inline-flex flex-wrap text-gray-600 space-x-1 md:space-x-3 items-center">
-        {list.map((object, id) => (
-          <li className="inline-flex items-center" key={id}>
-            <Link
-              href={object.url}
-              className="text-gray-600 hover:text-blue-600"
-            >
-              {object.name}
+export default function ({ list }: BreadCrumbsProps) {
+  return (
+    <nav aria-label="breadcrumb" className="p-8">
+      <ol className="flex items-center gap-1 text-base text-gray-700 max-w-screen-xl mx-auto px-4">
+        <li className="flex gap-1 items-center justify-center">
+          <Link href="/" className="block transition hover:text-blue-600">
+            <span className="sr-only"> Home </span>
+            <HomeIcon className="h-5 w-5" />
+          </Link>
+          <ArrowRightIcon className="h-4 w-4" />
+        </li>
+
+        {list.map((u, i) => (
+          <li className="flex gap-1 items-center justify-center" key={u.url}>
+            <Link href={u.url} className="block transition hover:text-black">
+              {u.name}
             </Link>
-            <i className="ml-3 text-gray-400 fa fa-chevron-right"></i>
+            {list.length > i + 1 && <ArrowRightIcon className="h-4 w-4" />}
           </li>
         ))}
       </ol>
-    </div>
-  </section>
-);
-
-export default BreadCrumbs;
+    </nav>
+  );
+}
