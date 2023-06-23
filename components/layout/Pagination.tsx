209799bee32af347f2576aeeb37dbb24c8a65b31
al-imam <alimam01828@gmail.com>
2023-06-23 15:53:53 +0600

    add arrow icons for pagination component

--- components/layout/Pagination.tsx
+++ components/layout/Pagination.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ArrowLeftIcon, ArrowRightIcon } from "$svg/icons";
 import { useRouter, useSearchParams } from "next/navigation";
 import ReactJsPagination from "react-js-pagination";
 
@@ -34,34 +35,8 @@ export default function ({ total, single, path = "" }: PaginationProps) {
       totalItemsCount={total}
       onChange={changePage}
       innerClass="flex justify-center gap-1 text-xs font-medium"
-      nextPageText={
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-3 w-3"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      }
-      prevPageText={
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-3 w-3"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      }
+      nextPageText={<ArrowRightIcon className="h-3 w-3" />}
+      prevPageText={<ArrowLeftIcon className="h-3 w-3" />}
       itemClassNext="inline-flex items-center justify-center"
       itemClassPrev="inline-flex items-center justify-center"
       itemClass="block h-8 w-8 rounded border border-gray-100 text-center leading-8 cursor-pointer"
--- svg/icons.tsx
+++ svg/icons.tsx
@@ -64,3 +64,41 @@ export function LogoutIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function ArrowRightIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 20 20"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        fill="currentColor"
+        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function ArrowLeftIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 20 20"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        fill="currentColor"
+        clipRule="evenodd"
+        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+      />
+    </svg>
+  );
+}
