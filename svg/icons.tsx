172914da4f49a6d910a43af107cf0e2842680dfb
al-imam <alimam01828@gmail.com>
2023-06-24 22:28:12 +0600

    add password write icon

--- svg/icons.tsx
+++ svg/icons.tsx
@@ -1,21 +1,8 @@
 import { SVGProps } from "react";
 
-export function CartIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 16 16"
-      {...props}
-    >
-      <path
-        fill="currentColor"
-        d="M2.5 2a.5.5 0 0 0 0 1h.246a.5.5 0 0 1 .48.363l1.586 5.55A1.5 1.5 0 0 0 6.254 10h4.569a1.5 1.5 0 0 0 1.393-.943l1.474-3.686A1 1 0 0 0 12.762 4H4.448l-.261-.912A1.5 1.5 0 0 0 2.746 2H2.5Zm3.274 6.637L4.734 5h8.027l-1.474 3.686a.5.5 0 0 1-.464.314H6.254a.5.5 0 0 1-.48-.363ZM6.5 14a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3Zm0-1a.5.5 0 1 1 0-1a.5.5 0 0 1 0 1Zm4 1a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3Zm0-1a.5.5 0 1 1 0-1a.5.5 0 0 1 0 1Z"
-      />
-    </svg>
-  );
-}
+export * from "./arrow";
+export * from "./user";
+export * from "./cart";
 
 export function SettingIcon(props: SVGProps<SVGSVGElement>) {
   return (
@@ -65,44 +52,6 @@ export function LogoutIcon(props: SVGProps<SVGSVGElement>) {
   );
 }
 
-export function ArrowRightIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 20 20"
-      {...props}
-    >
-      <path
-        fillRule="evenodd"
-        fill="currentColor"
-        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
-
-export function ArrowLeftIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 20 20"
-      {...props}
-    >
-      <path
-        fillRule="evenodd"
-        fill="currentColor"
-        clipRule="evenodd"
-        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-      />
-    </svg>
-  );
-}
-
 export function HomeIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
@@ -121,6 +70,28 @@ export function HomeIcon(props: SVGProps<SVGSVGElement>) {
       >
         <path d="m1.5 10.5l9-9l9 9" />
         <path d="M3.5 8.5v8a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-8" />
+      </g>
+    </svg>
+  );
+}
+
+export function PasswordWriteIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 14 14"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M5.62 7.38L11.5 1.5l2 2m-4.25.25L11 5.5"></path>
+        <circle cx="3.5" cy="9.5" r="3"></circle>
       </g>
     </svg>
   );
