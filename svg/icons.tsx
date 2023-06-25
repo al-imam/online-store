2b38fd669a0831eec2837be5501e03ef95ab8b8f
al-imam <alimam01828@gmail.com>
2023-06-25 16:01:33 +0600

    install radix dropdown and add dot icon

--- svg/icons.tsx
+++ svg/icons.tsx
@@ -96,3 +96,20 @@ export function PasswordWriteIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function ThreeDotIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M156 128a28 28 0 1 1-28-28a28 28 0 0 1 28 28Zm-28-52a28 28 0 1 0-28-28a28 28 0 0 0 28 28Zm0 104a28 28 0 1 0 28 28a28 28 0 0 0-28-28Z"
+      ></path>
+    </svg>
+  );
+}
