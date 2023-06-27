2e8d0a0c4c8a0785a48a1abd914d7dd4c7bec3af
al-imam <alimam01828@gmail.com>
2023-06-27 21:19:27 +0600

    refactor Price component to work with radix

--- components/filters/Price.tsx
+++ components/filters/Price.tsx
@@ -6,7 +6,7 @@ import useObjectStore from "use-object-store";
 
 const num = /(^\d*\.?\d*[0-9]+\d*$)|(^[0-9]+\d*\.\d*$)/;
 
-export default function () {
+export function Price() {
   const [{ min, max }, dispatch] = useObjectStore({ min: "", max: "" });
   const router = useRouter();
 
@@ -67,85 +67,59 @@ export default function () {
   }
 
   return (
-    <details className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
-      <summary className="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-        <span className="text-sm font-medium"> Price </span>
-
-        <span className="transition group-open:-rotate-180">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-4 w-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
+    <div className="border-t border-gray-200 bg-white">
+      <header className="flex items-center justify-between p-4">
+        <span className="text-sm text-gray-700">
+          The highest price is $6000
         </span>
-      </summary>
 
-      <div className="border-t border-gray-200 bg-white">
-        <header className="flex items-center justify-between p-4">
-          <span className="text-sm text-gray-700">
-            The highest price is $6000
-          </span>
+        <button
+          type="button"
+          onClick={reset}
+          className="text-sm text-gray-900 underline underline-offset-4"
+        >
+          Reset
+        </button>
+      </header>
 
-          <button
-            type="button"
-            onClick={reset}
-            className="text-sm text-gray-900 underline underline-offset-4"
+      <div className="border-t border-gray-200 p-4">
+        <div className="flex justify-between gap-4">
+          <label
+            htmlFor="filter-price-from"
+            className="flex items-center gap-2"
           >
-            Reset
-          </button>
-        </header>
+            <span className="text-sm text-gray-600">$</span>
 
-        <div className="border-t border-gray-200 p-4">
-          <div className="flex justify-between gap-4">
-            <label
-              htmlFor="filter-price-from"
-              className="flex items-center gap-2"
-            >
-              <span className="text-sm text-gray-600">$</span>
+            <input
+              type="number"
+              id="filter-price-from"
+              placeholder="From"
+              className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm form-input"
+              value={min}
+              onChange={validateNumber((value) => {
+                dispatch({ min: value });
+                changePriceQuery(value, max);
+              })}
+            />
+          </label>
 
-              <input
-                type="number"
-                id="filter-price-from"
-                placeholder="From"
-                className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm form-input"
-                value={min}
-                onChange={validateNumber((value) => {
-                  dispatch({ min: value });
-                  changePriceQuery(value, max);
-                })}
-              />
-            </label>
+          <label htmlFor="filter-price-to" className="flex items-center gap-2">
+            <span className="text-sm text-gray-600">$</span>
 
-            <label
-              htmlFor="filter-price-to"
-              className="flex items-center gap-2"
-            >
-              <span className="text-sm text-gray-600">$</span>
-
-              <input
-                type="number"
-                id="filter-price-to"
-                placeholder="To"
-                className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm form-input"
-                value={max}
-                onChange={validateNumber((value) => {
-                  dispatch({ max: value });
-                  changePriceQuery(min, value);
-                })}
-              />
-            </label>
-          </div>
+            <input
+              type="number"
+              id="filter-price-to"
+              placeholder="To"
+              className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm form-input"
+              value={max}
+              onChange={validateNumber((value) => {
+                dispatch({ max: value });
+                changePriceQuery(min, value);
+              })}
+            />
+          </label>
         </div>
       </div>
-    </details>
+    </div>
   );
 }
