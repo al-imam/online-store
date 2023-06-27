02edad15af7e50a14d3c9608897a6c5a7ffb14b4
al-imam <alimam01828@gmail.com>
2023-06-27 21:18:56 +0600

    refactor availability component to work with radix

--- components/filters/Availability.tsx
+++ components/filters/Availability.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function () {
+export function Availability() {
   const router = useRouter();
 
   const [availability, setAvailability] = useState<
@@ -38,91 +38,68 @@ export default function () {
   }, []);
 
   return (
-    <details className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
-      <summary className="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-        <span className="text-sm font-medium"> Availability </span>
-
-        <span className="transition group-open:-rotate-180">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-4 w-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
+    <div className="border-t border-gray-200">
+      <header className="flex items-center justify-between p-4">
+        <span className="text-sm text-gray-700 select-none">
+          {availability
+            ? availability === "available"
+              ? "In stock"
+              : "Out of stock"
+            : "Not selected"}
         </span>
-      </summary>
 
-      <div className="border-t border-gray-200 bg-white">
-        <header className="flex items-center justify-between p-4">
-          <span className="text-sm text-gray-700 select-none">
-            {availability
-              ? availability === "available"
-                ? "In stock"
-                : "Out of stock"
-              : "Not selected"}
-          </span>
+        <button
+          type="button"
+          onClick={() => changeQuery(availability as any)}
+          className="text-sm text-gray-900 underline underline-offset-4 select-none"
+        >
+          Reset
+        </button>
+      </header>
 
-          <button
-            type="button"
-            onClick={() => changeQuery(availability as any)}
-            className="text-sm text-gray-900 underline underline-offset-4 select-none"
+      <ul className="space-y-1 border-t border-gray-200 p-4">
+        <li>
+          <label
+            htmlFor="filter-in-stock"
+            className="inline-flex items-center gap-2"
           >
-            Reset
-          </button>
-        </header>
+            <input
+              type="radio"
+              name="availability"
+              id="filter-in-stock"
+              value="available"
+              checked={"available" === availability}
+              onChange={(e) => changeQuery(e.target.value as "available")}
+              className="form-radio h-5 w-5 rounded border-gray-300"
+            />
 
-        <ul className="space-y-1 border-t border-gray-200 p-4">
-          <li>
-            <label
-              htmlFor="filter-in-stock"
-              className="inline-flex items-center gap-2"
-            >
-              <input
-                type="radio"
-                name="availability"
-                id="filter-in-stock"
-                value="available"
-                checked={"available" === availability}
-                onChange={(e) => changeQuery(e.target.value as "available")}
-                className="form-radio h-5 w-5 rounded border-gray-300"
-              />
+            <span className="text-sm font-medium text-gray-700 select-none">
+              In Stock (5+)
+            </span>
+          </label>
+        </li>
 
-              <span className="text-sm font-medium text-gray-700 select-none">
-                In Stock (5+)
-              </span>
-            </label>
-          </li>
+        <li>
+          <label
+            htmlFor="filter-out-of-stock"
+            className="inline-flex items-center gap-2"
+          >
+            <input
+              type="radio"
+              id="filter-out-of-stock"
+              value="unavailable"
+              checked={"unavailable" === availability}
+              onChange={(e) => changeQuery(e.target.value as "unavailable")}
+              name="availability"
+              className="form-radio h-5 w-5 rounded border-gray-300"
+            />
 
-          <li>
-            <label
-              htmlFor="filter-out-of-stock"
-              className="inline-flex items-center gap-2"
-            >
-              <input
-                type="radio"
-                id="filter-out-of-stock"
-                value="unavailable"
-                checked={"unavailable" === availability}
-                onChange={(e) => changeQuery(e.target.value as "unavailable")}
-                name="availability"
-                className="form-radio h-5 w-5 rounded border-gray-300"
-              />
-
-              <span className="text-sm font-medium text-gray-700 select-none">
-                Out of Stock (10+)
-              </span>
-            </label>
-          </li>
-        </ul>
-      </div>
-    </details>
+            <span className="text-sm font-medium text-gray-700 select-none">
+              Out of Stock (10+)
+            </span>
+          </label>
+        </li>
+      </ul>
+    </div>
   );
 }
