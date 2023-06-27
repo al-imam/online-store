2e6f8dae98d31f3c42c92b9f471b1230dd66e9db
al-imam <alimam01828@gmail.com>
2023-06-27 21:18:12 +0600

    auto slide product items images

--- components/products/productItem.tsx
+++ components/products/productItem.tsx
@@ -1,38 +1,48 @@
+"use client";
+
 import { AddCart } from "$components/utility";
 import ProductInterface from "$types/productInterface";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
 
 interface ProductProps {
   item: ProductInterface;
 }
 
+function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export function Product({ item }: ProductProps) {
   return (
-    <li className="group relative block overflow-hidden">
-      <button className="absolute end-4 top-4 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
-        <span className="sr-only">Wishlist</span>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-4 w-4"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-          />
-        </svg>
-      </button>
-
-      <img
-        src={item.images[0] ? item.images[0].url : "/product.png"}
-        alt={item.name}
-        className="h-64 w-full object-contain transition duration-500 group-hover:scale-105"
-      />
+    <li className="relative block overflow-hidden">
+      <Link href={`/product/${item._id}`} draggable={false}>
+        <div className="relative hover:overlay-effect ">
+          <Swiper
+            loop
+            pagination={false}
+            scrollbar={false}
+            modules={[Autoplay]}
+            autoplay={{
+              delay: getRandomNumber(2000, 5000),
+              pauseOnMouseEnter: true,
+            }}
+          >
+            {item.images.map((image) => (
+              <SwiperSlide>
+                <img
+                  src={image.url}
+                  alt={item.name}
+                  className="h-64 w-full object-contain "
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </Link>
 
       <div className="relative border border-gray-100 bg-white p-6">
         <Link
