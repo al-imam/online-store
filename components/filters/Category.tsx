156716b563e2e4f9bb9889c69368c17fd791ceca
al-imam <alimam01828@gmail.com>
2023-06-28 09:20:18 +0600

    refactor Category component to work with radix

--- components/filters/Category.tsx
+++ components/filters/Category.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useId } from "react";
 import useObjectStore from "use-object-store";
 
-export default function () {
+export function Category() {
   const router = useRouter();
   const [store, updateStore] = useObjectStore({
     category: null as string | null,
@@ -38,58 +38,32 @@ export default function () {
   }, []);
 
   return (
-    <details
-      open={true}
-      className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
-    >
-      <summary className="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-        <span className="text-sm font-medium"> Category </span>
-
-        <span className="transition group-open:-rotate-180">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-4 w-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
+    <div className="border-t border-gray-200">
+      <header className="flex items-center justify-between p-4">
+        <span className="text-sm text-gray-700">
+          {store.category ?? "Not selected"}
         </span>
-      </summary>
 
-      <div className="border-t border-gray-200 bg-white">
-        <header className="flex items-center justify-between p-4">
-          <span className="text-sm text-gray-700">
-            {store.category ?? "Not selected"}
-          </span>
+        <button
+          type="button"
+          className="text-sm text-gray-900 underline underline-offset-4"
+          onClick={() => store.category && changeQuery(store.category)}
+        >
+          Reset
+        </button>
+      </header>
 
-          <button
-            type="button"
-            className="text-sm text-gray-900 underline underline-offset-4"
-            onClick={() => store.category && changeQuery(store.category)}
-          >
-            Reset
-          </button>
-        </header>
-
-        <ul className="space-y-1 border-t border-gray-200 p-4">
-          {categories.map((category) => (
-            <Item
-              key={category}
-              category={category}
-              selected={store.category}
-              changeQuery={changeQuery}
-            />
-          ))}
-        </ul>
-      </div>
-    </details>
+      <ul className="space-y-1 border-t border-gray-200 p-4">
+        {categories.map((category) => (
+          <Item
+            key={category}
+            category={category}
+            selected={store.category}
+            changeQuery={changeQuery}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
